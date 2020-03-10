57d4f30171243ee9f109f74747e564920fc23d28
rom <romvargas81@gmail.com>
2020-03-10 11:45:21 -0600

    se agrega nexus al pom y constructor publico en config de Token

--- wsmt-backend/src/main/java/com/nglobal/status/management/config/JwtTokenConfig.java
+++ wsmt-backend/src/main/java/com/nglobal/status/management/config/JwtTokenConfig.java
@@ -3,4 +3,11 @@ package com.nglobal.status.management.config;
 public class JwtTokenConfig {
 
 	public static final String SECRET_KEY = "NGLOBAL.Seceret.Key";
+
+	private JwtTokenConfig() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
